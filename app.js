b7d06abba95d1f7c703cb427fdf8bb13969b7a0f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-16 09:06:58 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const app = express()
+const express = require('express')
+const app = express() 
 
-// pet-listings
+// /pet-listing 
 app.get('/pet-listing', (req, res) => {
-	
-	const model = {
-		pageTile: 'Pets',
-		components: [
-			{
-				type: 'featuredImage',
-				data: {
-					imageUrl: "https://unsplash.com/photos/gKXKBY-C-Dk"
-				}
-			}
-		]
-	}
-	
-	res.json(model)
+
+    const model = {
+        pageTitle: 'Pets', 
+        components: [
+            {
+                type: "featuredImage", 
+                data: {
+                    imageUrl: "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1468&q=80"
+                }
+            }
+        ]
+    }
+
+    res.json(model)
 })
 
 app.listen(3000, () => {
-	console.log("Server is running on 3000")
+    console.log('Server is running...')
 })
