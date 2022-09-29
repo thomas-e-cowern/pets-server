1531edeb968196e15c08091512f4bc33bae103e9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-29 16:18:03 -0400

    updated json to remove title

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.get('/pet-listing', (req, res) => {
                     {
                         id: 2,
                         title: "scary",
-                        subTitle: "This is a great picture",
+//                      subTitle: "This is a great picture",
                         imageUrl: "https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=904&q=80",
                         description: "The description goes here..."
                     },
